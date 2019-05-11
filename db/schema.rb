1039edafe011d474f6168aa3e6e4c7e79d2ab65c
e0 <shan.huang@me.com>
2019-05-11 13:56:56 +0200

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_105208) do
+ActiveRecord::Schema.define(version: 2019_05_11_112821) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,13 @@ ActiveRecord::Schema.define(version: 2019_05_11_105208) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "department_id"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.index ["department_id"], name: "index_users_on_department_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
