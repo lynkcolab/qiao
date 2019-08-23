61a17ce54d7f15ab6d18ecf82e9f0683efa1bc5e
william.andersson <z8william.andersson@cevt.se>
2019-08-23 15:12:44 +0200

    Email production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'qiao.herokuapp.com' }
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.default_url_options = { :host => 'qiao.herokuapp.com' }
+  #config.action_mailer.perform_deliveries = true
+
+  #config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'http://52.136.215.196/' #replace with your own url
+  config.action_mailer.default_url_options = { :host => 'http://52.136.215.196/', protocol: 'http' }
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "absurditybycolab@gmail.com",
+    :password             => "Itswintertime!",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+}
+  
 end
